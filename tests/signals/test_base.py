c1f147ccef47d4f8991378f969cbb08c352f666b
Greg Leclercq <greg@0x80.net>
2014-04-15 22:50:47 +0200

    Fix tests.signals.test_base: move to pytest

--- tests/signals/test_base.py
+++ tests/signals/test_base.py
@@ -1,11 +1,15 @@
-import unittest
+import pytest
 import collections
 
 from pkit.signals import base
+from pkit.signals import constants
 
 
-class TestBase(unittest.TestCase):
-    def setUp(self):
+class TestBase(object):
+    def setup_method(self):
+        base.SIGNAL_HANDLERS = collections.defaultdict(list)
+
+    def teardown_method(self):
         base.SIGNAL_HANDLERS = collections.defaultdict(list)
 
 
@@ -13,30 +17,31 @@ class TestCallSignalHandler(TestBase):
     def test_call_signal_handler(self):
         result = [None]
 
-        def handler(string, *args):
-            result[0] = '{} works.'.format(string)
+        def handler(signum, sigframe):
+            result[0] = '{} works.'.format(signum)
 
-        base.SIGNAL_HANDLERS[base.signal.SIGTERM] = [handler]
-        base.call_signal_handler(base.signal.SIGTERM)('test')
-        self.assertEquals(result[0], 'test works.')
+        base.SIGNAL_HANDLERS[constants.SIGTERM] = [handler]
+        base.call_signal_handler(constants.SIGTERM)(
+
+            constants.SIGTERM, None)
+        result[0] == '{} works.'.format(constants.SIGTERM)
 
 
 class TestRegister(TestBase):
     def test_register_unknown_signum_raises(self):
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             base.register('aint no signal', lambda: None)
 
     def test_register_handler_with_invalid_type(self):
-        with self.assertRaises(TypeError):
-            base.register(base.signal.SIGTERM, 'not a function')
+        with pytest.raises(TypeError):
+            base.register(constants.SIGTERM, 'not a function')
 
     def test_register_one_handler(self):
         def handler(*args):
             return
 
-        base.register(base.signal.SIGTERM, handler)
-        self.assertEquals(base.SIGNAL_HANDLERS[base.signal.SIGTERM],
-                          [handler])
+        base.register(constants.SIGTERM, handler)
+        base.SIGNAL_HANDLERS[constants.SIGTERM] == [handler]
 
     def test_register_two_handlers(self):
         def handler1(*args):
@@ -45,22 +50,24 @@ class TestRegister(TestBase):
         def handler2(*args):
             return
 
-        base.register(base.signal.SIGTERM, handler1)
-        base.register(base.signal.SIGTERM, handler2)
+        base.register(constants.SIGTERM, handler1)
+        base.register(constants.SIGTERM, handler2)
 
-        self.assertEquals(base.SIGNAL_HANDLERS[base.signal.SIGTERM],
-                          [handler1, handler2])
+        base.SIGNAL_HANDLERS[constants.SIGTERM] = [handler1, handler2]
 
 
-class TestUnregister(unittest.TestCase):
+class TestUnregister(object):
+    def setup_module(self):
+        base.SIGNAL_HANDLERS = collections.defaultdict(list)
+
     def test_unregister_one_handler(self):
         def handler(*args):
             return
 
-        base.register(base.signal.SIGTERM, handler)
-        base.unregister(base.signal.SIGTERM, handler)
-        self.assertEquals(base.SIGNAL_HANDLERS[base.signal.SIGTERM],
-                          [])
+        base.register(constants.SIGTERM, handler)
+
+        base.unregister(constants.SIGTERM, handler)
+        base.SIGNAL_HANDLERS[constants.SIGTERM] == []
 
     def test_unregister_two_handlers(self):
         def handler1(*args):
@@ -69,26 +76,24 @@ class TestUnregister(unittest.TestCase):
         def handler2(*args):
             return
 
-        base.register(base.signal.SIGTERM, handler1)
-        base.register(base.signal.SIGTERM, handler2)
+        base.register(constants.SIGTERM, handler1)
+        base.register(constants.SIGTERM, handler2)
 
-        base.unregister(base.signal.SIGTERM, handler1)
-        self.assertEquals(base.SIGNAL_HANDLERS[base.signal.SIGTERM],
-                          [handler2])
+        base.unregister(constants.SIGTERM, handler1)
+        base.SIGNAL_HANDLERS[constants.SIGTERM] == [handler2]
 
-        base.unregister(base.signal.SIGTERM, handler2)
-        self.assertEquals(base.SIGNAL_HANDLERS[base.signal.SIGTERM],
-                          [])
+        base.unregister(constants.SIGTERM, handler2)
+        base.SIGNAL_HANDLERS[constants.SIGTERM] == []
 
     def test_unregister_signal_not_found(self):
-        with self.assertRaises(LookupError):
-            base.unregister(base.signal.SIGTERM, 'test')
+        with pytest.raises(LookupError):
+            base.unregister(constants.SIGTERM, 'test')
 
     def test_unregister_handler_not_found(self):
         def handler(*args):
             return
 
-        base.register(base.signal.SIGTERM, handler)
+        base.register(constants.SIGTERM, handler)
 
-        with self.assertRaises(LookupError):
-            base.unregister(base.signal.SIGTERM, 'test')
+        with pytest.raises(LookupError):
+            base.unregister(constants.SIGTERM, 'test')
