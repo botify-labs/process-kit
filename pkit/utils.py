4ad8a7ca99dfac945b794371696b043a493ac1bd
Greg Leclercq <greg@0x80.net>
2014-04-15 22:50:07 +0200

    Update utils.wait()

--- pkit/utils.py
+++ pkit/utils.py
@@ -3,16 +3,20 @@ import time
 from pkit.exceptions import TimeoutError
 
 
+DEFAULT_INTERVAL = 0.005
+
+
 def wait(until=None, timeout=None, args=(), kwargs={}):
     if not callable(until):
         raise TypeError("until must be callable")
 
     elapsed = 0.0
 
+    interval = DEFAULT_INTERVAL
     while until(*args, **kwargs) is False:
-        time.sleep(0.005)
-        elapsed += 0.005
-        
+        time.sleep(interval)
+        elapsed += interval
+
         if timeout is not None and elapsed >= timeout:
             raise TimeoutError
 
